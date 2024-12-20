c1d3a416607b28cf4e5381cfa740bff83a55029a
mahmoud.ali <mahmoud.ali@dascad.onmicrosoft.com>
2024-12-20 21:43:10 +0200

    last

--- projects/ngx-hijri-datepicker/src/public-api.ts
+++ projects/ngx-hijri-datepicker/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-hijri-datepicker
  */
 
-export * from './lib/ngx-hijri-datepicker.service';
-export * from './lib/ngx-hijri-datepicker.component';
 export * from './lib/ngx-hijri-datepicker.module';
+export * from './lib/ngx-hijri-datepicker.component';
